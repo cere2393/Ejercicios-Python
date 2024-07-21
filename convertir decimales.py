d64560ba4e4f739434002b6ca567f23154ee3398
cere2393 <carlos.ramirez@tecnologicoliceocristiano.edu.ec>
2024-07-21 18:14:02 -0500

    Add files via upload

---
+++ convertir decimales.py
@@ -0,0 +1,5 @@
+ValorDecimal=float(input("ingrese primer valor decimal:"))
+ValorEntero=int(input("ingrese un valor entero:"))
+
+Suma=ValorDecimal+ValorEntero
+print("el rersultado de la suma es",Suma)
